422c671a9b76bb1f88c9ee4323d3f8a5045e48f6
Edy Galdiolo <edygaldy302@gmail.com>
2023-06-20 15:51:39 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+class Movie {
+    public $title;
+    public $poster;
+    public $producer;
+    public $director;
+    public $diration;
+    public $budget;
+    public $year;
+    public $language;
+
+}
+
+?>
